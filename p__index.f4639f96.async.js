5229843a381c884ea82ba225b282ee07804e1b7c
Howard.Zuo <leftstick@qq.com>
2019-06-09 16:15:54 +0800

    Updates

---
+++ p__index.f4639f96.async.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[3],{RXBc:function(e,t,a){"use strict";a.r(t);var n=a("vEW2"),d=a.n(n),c=a("dPBl"),o=a.n(c);t["default"]=(()=>d.a.createElement(o.a,{to:{pathname:"/game"}}))},dPBl:function(e,t,a){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t.default=void 0;var n=a("eO8H"),d=n.Redirect;t.default=d}}]);
